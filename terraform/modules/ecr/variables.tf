f8b22e355b7d41a6ae47c8cb4bb76c5c1a27e86e
Artem Chernenko <artem.chernenko@betterme.world>
2026-03-20 17:31:25 +0200

    final project

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,4 +1,18 @@
-variable "repository_name"    { type = string }
-variable "max_image_count"    { type = number default = 10 }
-variable "push_principal_arns" { type = list(string) default = [] }
-variable "common_tags"        { type = map(string) default = {} }
+variable "repository_name" {
+  type = string
+}
+
+variable "max_image_count" {
+  type    = number
+  default = 10
+}
+
+variable "push_principal_arns" {
+  type    = list(string)
+  default = []
+}
+
+variable "common_tags" {
+  type    = map(string)
+  default = {}
+}
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,5 +1,20 @@
-variable "project_name" { type = string }
-variable "vpc_cidr"     { type = string }
-variable "public_subnet_cidrs"  { type = list(string) }
-variable "private_subnet_cidrs" { type = list(string) }
-variable "common_tags"  { type = map(string) default = {} }
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "common_tags" {
+  type    = map(string)
+  default = {}
+}
