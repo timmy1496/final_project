8f1c0e86c43426c79b2547c6937c5895d3f8ebcf
Artem Chernenko <artem.chernenko@betterme.world>
2026-03-20 17:22:28 +0200

    final project

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,6 +1,6 @@
-variable "aws_region"   { type = string; default = "eu-central-1" }
-variable "environment"  { type = string; default = "dev" }
-variable "project_name" { type = string; default = "go-app" }
+variable "aws_region"   { type = string default = "eu-central-1" }
+variable "environment"  { type = string default = "dev" }
+variable "project_name" { type = string default = "go-app" }
 
 variable "vpc_cidr" {
   type    = string
@@ -15,15 +15,15 @@ variable "private_subnet_cidrs" {
   default = ["10.0.10.0/24", "10.0.11.0/24"]
 }
 
-variable "kubernetes_version"  { type = string; default = "1.29" }
-variable "node_instance_types" { type = list(string); default = ["t3.medium"] }
-variable "node_desired_size"   { type = number; default = 2 }
-variable "node_min_size"       { type = number; default = 1 }
-variable "node_max_size"       { type = number; default = 4 }
-variable "ec2_ssh_key_name"    { type = string; default = "" }
+variable "kubernetes_version"  { type = string default = "1.29" }
+variable "node_instance_types" { type = list(string) default = ["t3.medium"] }
+variable "node_desired_size"   { type = number default = 2 }
+variable "node_min_size"       { type = number default = 1 }
+variable "node_max_size"       { type = number default = 4 }
+variable "ec2_ssh_key_name"    { type = string default = "" }
 
-variable "rds_instance_class"  { type = string; default = "db.t3.micro" }
-variable "db_name"             { type = string; default = "appdb" }
+variable "rds_instance_class"  { type = string default = "db.t3.micro" }
+variable "db_name"             { type = string default = "appdb" }
 
 variable "common_tags" {
   type = map(string)
