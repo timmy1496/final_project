04d89651adfcafc274067cbdabb5ff99d224ee23
Artem Chernenko <artem.chernenko@betterme.world>
2026-03-20 18:52:46 +0200

    test

---
+++ terraform/environments/dev/github-oidc.tf
@@ -0,0 +1,37 @@
+# Використовуємо вже існуючий OIDC provider (створений вручну)
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-go-app"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = data.aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_USERNAME/go-k8s-infra:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  description = "Paste this into GitHub Secrets as AWS_ROLE_ARN"
+  value       = aws_iam_role.github_actions.arn
+}
